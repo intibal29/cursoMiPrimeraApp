43eee9acac184abf2cbbe1f4c96a080c0c26b770
intibal29 <baloukintissar@gmail.com>
2024-10-22 09:56:06 +0200

    Clase añadido tareas 1 : variables, funciones y listas

--- app/src/main/java/com/example/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -6,15 +6,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+@Suppress("UNREACHABLE_CODE")
 class MainActivity : AppCompatActivity() {
+    //si se quiere iniciar una variable mas tarde es con lateinit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        test("hola")
     }
+    //funcion con parametro de entrada y que devuelve un string
+    fun test(saludo:String="adios" ): String{
+        val nombre:String="juan" //definimos tipo
+        val example=8.1; // sin definir tipo
+       return "hola$nombre"// esto es igual que hola+nombre
+        //val list:List<String> = listof("1","2") // listas mutables y no mutables
+       // val list2:MutableList<String> = mutaleListOf("1","2") // listas mutables
+          //  list2.add("hola")
+    }
+    fun test2(saludo:String="adios" ): String = "hola$nombre" // nesta funcion hace lo mismo que la de arriba
+        val nombre:String="juan" //definimos tipo
+
 }

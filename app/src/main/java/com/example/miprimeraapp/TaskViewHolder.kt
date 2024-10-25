76c224fa08c18a896460106ab5268aa624da849e
intibal29 <baloukintissar@gmail.com>
2024-10-25 08:59:58 +0200

    Clase listados RecyclerView 2 : Diseño celdas y ViewHolder

---
+++ app/src/main/java/com/example/miprimeraapp/TaskViewHolder.kt
@@ -0,0 +1,14 @@
+package com.example.miprimeraapp
+
+import android.view.View;
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+// recive una vista
+ class TaskViewHolder (view: View) : RecyclerView.ViewHolder(view){
+   private val tvTask:TextView=view.findViewById(R.id.textView)
+
+ fun render(task:String){
+   tvTask.text=task
+ }
+}

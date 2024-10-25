1af47e591aeeb7894d32f903e86646d8a0124559
intibal29 <baloukintissar@gmail.com>
2024-10-25 21:32:21 +0200

    tasks adapter111

--- app/src/main/java/com/example/miprimeraapp/TaskAdapter.kt
+++ app/src/main/java/com/example/miprimeraapp/TaskAdapter.kt
@@ -1,3 +1,22 @@
 package com.example.miprimeraapp
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskAdapter (val tasks:List<String>): RecyclerView.Adapter< TaskViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return TaskViewHolder(layoutInflater.inflate(R.layout.item_task,parent))
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.render(tasks[position])
+    }
+
+
+    override fun getItemCount()= tasks.size
+
+
+
 }

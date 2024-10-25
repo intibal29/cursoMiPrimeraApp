d63431ccb464e15c6df678f0dce1f4eb6e8c95ff
intibal29 <baloukintissar@gmail.com>
2024-10-25 23:04:33 +0200

    guardar informacion

--- app/src/main/java/com/example/miprimeraapp/Preferences.kt
+++ app/src/main/java/com/example/miprimeraapp/Preferences.kt
@@ -6,7 +6,11 @@ import android.content.SharedPreferences
 class Preferences (context: Context){
     companion object{
         const val  PREFS_NAME="myDatabase"
+        const val  TASKS="tasks_value"
     }
 
     val prefs: SharedPreferences =context.getSharedPreferences(PREFS_NAME,0)
+    fun saveTasks(tasks:List<String>){
+        prefs.edit().putStringSet(TASKS, tasks.toSet()).apply() //guardar un valor en una clave
+    }
 }

13be0fecdcb104fdecc726d2c9f9b64dec17b368
intibal29 <baloukintissar@gmail.com>
2024-10-25 08:35:31 +0200

    Clase añadido tareas 3 : Listeners

--- app/src/main/java/com/example/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -1,30 +1,47 @@
 package com.example.miprimeraapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
 
 @Suppress("UNREACHABLE_CODE")
 class MainActivity : AppCompatActivity() {
-    //si se quiere iniciar una variable mas tarde es con lateinit
+    lateinit var btnbutton:Button //variable button
+    lateinit var etText: EditText 
+    lateinit var rvText: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        test("hola")
+       iniUi()
     }
-    //funcion con parametro de entrada y que devuelve un string
-    fun test(saludo:String="adios" ): String{
-        val nombre:String="juan" //definimos tipo
-        val example=8.1; // sin definir tipo
-       return "hola$nombre"// esto es igual que hola+nombre
-        //val list:List<String> = listof("1","2") // listas mutables y no mutables
-       // val list2:MutableList<String> = mutaleListOf("1","2") // listas mutables
-          //  list2.add("hola")
-    }
-    fun test2(saludo:String="adios" ): String = "hola$nombre" // nesta funcion hace lo mismo que la de arriba
-        val nombre:String="juan" //definimos tipo
 
+    private fun iniUi() {
+        initView()//instanciar los valores
+        initListeners()
+    }
+    //listener
+    private fun initListeners() {
+       btnbutton.setOnClickListener(){
+           addbutoon()
+       }
+    }
+
+    private fun addbutoon() {
+       val taskToAdd: String = etText.text.toString()
+    }
+
+
+    private fun initView() {
+        btnbutton = findViewById(R.id.button)// esto hace que te busque la vista por id 
+        etText = findViewById(R.id.etTarea)
+        rvText = findViewById(R.id.rvT)
+        rvText = findViewById(R.id.rvT)
+    }
 }
